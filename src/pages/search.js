d4a7c8229bdcd6d5e6d317f502f34ff5fb1d7608
Janio_Souza <janiosouz001@gmail.com>
2022-08-31 21:48:01 -0300

    [REQ: 05] Em componente Search criado form com input e button e sua funcao de validacao

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,11 +2,44 @@ import React from 'react';
 import Header from '../components/header';
 
 export default class Search extends React.Component {
+  state = {
+    disabledSearch: true,
+    searchMusic: '',
+  };
+
+  searc_Btn = (element) => {
+    const MIN_OF_CARACT = 2;
+    this.setState({ searchMusic: element.target.value });
+    return (
+      element.target.value.length >= MIN_OF_CARACT
+        ? this.setState({ disabledSearch: false })
+        : this.setState({ disabledSearch: true })
+    );
+  };
+
   render() {
+    const { disabledSearch, searchMusic } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Teste search - Ok</h1>
+        <form>
+          <label htmlFor="search">
+            <input
+              type="text"
+              name="search"
+              data-testid="search-artist-input"
+              value={ searchMusic }
+              onChange={ this.searc_Btn }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ disabledSearch }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
